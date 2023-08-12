a3567930284aff10fbf84247a6a6248ac02a51b2
Mihail Marian <mihailmarian12@gmail.com>
2023-08-12 14:49:17 +0100

    wip

--- init.sh
+++ init.sh
@@ -29,6 +29,12 @@ yum install iptables-services -y
 systemctl enable iptables
 systemctl start iptables
 
+# Allow traffic to HTTP(S) and Jenkins ports
+iptables -I INPUT 1 -p tcp --dport 8443 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+
 # Forward traffic from HTTP(S) ports to the Jenkins ports
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
