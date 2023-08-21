dd7bfaafb387ff10e8caa648694df27343537c91
Mihail Marian <mihailmarian12@gmail.com>
2023-08-21 17:06:23 +0100

    WIP-5

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
-# Terraform / Jenkins / AWS EC2
+# Jenkins on AWS EC2 using Terraform
 
 Terraform scripts that deploy a Jenkins server on AWS EC2.
 
-## Choices
+## Architecture diagram
+
+TODO
+
+## Setup
+
+### 1. Backend
+
+Configure the `backend` in the [provider.tf](./provider.tf) file, which stores the state of your infrastructure.
+If you decide to keep the backend in S3, remember to create the bucket beforehand; ideally, you should also
+enable [bucket versioning](https://docs.aws.amazon.com/AmazonS3/latest/userguide/manage-versioning-examples.html)
+to allow for state recovery in case of any mistakes.
+
+### 2. Domain
+
+Register a domain with your preferred domain registrar, then set up a hosted zone in Route 53. Copy the 
+
+
+## Technical choices
+
+
 
 iptables instead of Nginx and the likes - simplicity
 iptables instead of Elastic Load Balancer - simplicity and cost
--- init.sh
+++ init.sh
@@ -5,45 +5,6 @@ sudo -i
 # Update packages
 apt-get update; apt-get full-upgrade -y; apt-get autoclean -y;
 
-### Forward traffic from HTTP(S) ports to the Jenkins ports
-
-# # Install nftables
-# apt-get install nftables -y
-
-# # Create routing ruleset file
-# cat <<EOF > nft_rules.nft
-# table ip nat {
-#     chain prerouting {
-#         type nat hook prerouting priority 0; policy accept;
-#         tcp dport 80 log prefix "HTTP: "  redirect to :8080
-#         tcp dport 443  log prefix "HTTPS: "  redirect to :8080
-#     }
-# }
-# EOF
-
-# cat > $nginx_config_path <<EOF
-# server {
-#     listen 443;
-#     server_name pipeline.employerintel.com;
-
-#     location / {
-#         proxy_pass http://127.0.0.1:8080;
-#         proxy_set_header Host $host;
-#         proxy_set_header X-Real-IP $remote_addr;
-#         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-#         proxy_set_header X-Forwarded-Proto $scheme;    }
-# }
-
-# EOF
-
-# # Load ruleset
-# nft -f nft_rules.nft
-
-# # Save ruleset so it works after reboot
-# nft list ruleset > /etc/nftables.conf
-
-### Set up Jenkins
-
 # Install Java
 apt-get install openjdk-17-jdk openjdk-17-jre -y
 
